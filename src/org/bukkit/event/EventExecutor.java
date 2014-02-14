f85bb6c424536317c85dc5b691d67fc501a617ab
Darius Clark <darius.clark@shouthost.org>
2014-02-13 19:21:45 -0500

    Removed git derp

--- src/org/bukkit/event/EventExecutor.java
+++
@@ -1,9 +0,0 @@
-package org.bukkit.event;
-
-
-/**
- * Interface which defines the class for event call backs to plugins
- */
-public interface EventExecutor {
-    public void execute(Listener listener, Event event) throws EventException;
-}
